8fe91a1ace53d19bcd0b01826fed37c2391ad084
unknown <mohamadmowfakhlal@gmail.com>
2021-04-11 17:34:42 +0300

    fix an error

--- src/main/java/org/acme/rest/json/Session.java
+++ src/main/java/org/acme/rest/json/Session.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.json.JSONObject;
 
 public class Session extends JSONObject{
-	UUID session;
+	public UUID session;
 	public void setUUID(UUID session) {
 		this.session = session;
 		
